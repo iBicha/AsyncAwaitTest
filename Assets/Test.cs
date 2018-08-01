45a4b121db80bb53009ecaf55b253d517c7ef3cb
Brahim Hadriche <brahim.hadriche@gmail.com>
2018-08-01 10:23:48 -0600

    Project test

---
+++ Assets/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	private async void Start()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			Debug.Log(Time.frameCount);
+			//A call to EmptyTask() should last at least 1 frame, according to how the SynchronizationContext should work
+			//But the first call returns immediately, which means it was run in a synchronous manner
+			await EmptyTask();
+		}
+		
+		/*
+		 * Expected output:
+		 * 1
+		 * 2
+		 * 3
+		 * 4
+		 * 5
+		 *
+		 *
+		 *
+		 * Actual output:
+		 * 1
+		 * 1
+		 * 2
+		 * 3
+		 * 4
+		 */
+
+	}
+
+	async Task EmptyTask()
+	{
+		await Task.Run(() => { });
+	}
+}
